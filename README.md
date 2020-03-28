0d41da2f4b603e5aabc94353748cf8e085f0052c
Aaron Godfrey <me@aarongodfrey.dev>
2020-03-28 13:35:22 -0700

    Fix access to config entry.
    Also provided a basic description in the README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Steam Wishlist
+# Steam Wishlist for Home Assistant
+
+A custom component that keeps track of when games on your STEAM wishlist are on
+sale.
--- custom_components/steam_wishlist/__init__.py
+++ custom_components/steam_wishlist/__init__.py
@@ -13,7 +13,7 @@ DATA_CONFIGS = "steam_wishlist_config"
 async def async_setup_entry(
     hass: core.HomeAssistant, entry: config_entries.ConfigEntry
 ) -> bool:
-    _LOGGER.info("async_setup_entry: setup url %s", entry["url"])
+    _LOGGER.info("async_setup_entry: setup url %s", entry.data["url"])
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
