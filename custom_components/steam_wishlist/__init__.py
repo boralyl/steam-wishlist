3c0bf9a49465ca84e93ba49a01f6e4ce04dc3ea1
Aaron Godfrey <me@aarongodfrey.dev>
2020-03-28 13:30:50 -0700

    Add required setup method.

--- custom_components/steam_wishlist/__init__.py
+++ custom_components/steam_wishlist/__init__.py
@@ -4,8 +4,10 @@ import logging
 
 from homeassistant import config_entries, core
 
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+DATA_CONFIGS = "steam_wishlist_config"
 
 
 async def async_setup_entry(
@@ -15,4 +17,23 @@ async def async_setup_entry(
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
+    return True
+
+
+async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
+    """Set up the STEAM wishlist platform."""
+    conf = config.get(DOMAIN)
+    if conf is None:
+        conf = {}
+
+    hass.data[DOMAIN] = {}
+    hass.data[DATA_CONFIGS] = conf
+
+    if conf is not None:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN, context={"source": config_entries.SOURCE_IMPORT}, data=conf
+            )
+        )
+
     return True
