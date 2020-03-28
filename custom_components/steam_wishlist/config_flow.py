26459c696bafe5eb2feff0673fd37789d2d3caca
Aaron Godfrey <me@aarongodfrey.dev>
2020-03-28 13:14:35 -0700

    Removed default value that had an error

--- custom_components/steam_wishlist/config_flow.py
+++ custom_components/steam_wishlist/config_flow.py
@@ -14,10 +14,7 @@ class SteamWishlistConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title="STEAM Wishlist", data=user_input,)
 
         return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema(
-                {vol.Required("url", default=self.config_entry.options.get("url")): str}
-            ),
+            step_id="user", data_schema=vol.Schema({vol.Required("url"): str}),
         )
 
     async def async_step_import(self, import_config):
