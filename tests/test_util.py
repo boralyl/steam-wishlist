3678a3acaf6c48a2f54a8af679fb6a93371aedd2
Aaron Godfrey <me@aarongodfrey.dev>
2020-05-28 20:03:35 -0700

    Better test coverage for util.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -23,3 +23,32 @@ def test_get_steam_game_unreleased_game():
         "title": "Control",
     }
     assert expected == actual
+
+
+def test_get_steam_game_with_sale_price():
+    """Test that we get the correct return value for a normal on sale game."""
+    game_id = "975150"
+    game = {
+        "name": "Resolutiion",
+        "capsule": "https://steamcdn-a.akamaihd.net/steam/apps/975150/header_292x136.jpg?t=1590678003",
+        "subs": [
+            {
+                "id": 320073,
+                "discount_block": '<div class="discount_block discount_block_large" data-price-final="1699"><div class="discount_pct">-15%</div><div class="discount_prices"><div class="discount_original_price">$19.99</div><div class="discount_final_price">$16.99</div></div></div>',
+                "discount_pct": 15,
+                "price": 1699,
+            }
+        ],
+        "type": "Game",
+        "added": 1590721175,
+    }
+    actual = get_steam_game(game_id, game)
+    expected = {
+        "box_art_url": "https://steamcdn-a.akamaihd.net/steam/apps/975150/header_292x136.jpg?t=1590678003",
+        "normal_price": 19.99,
+        "percent_off": 15,
+        "sale_price": 16.990000000000002,
+        "steam_id": "975150",
+        "title": "Resolutiion",
+    }
+    assert expected == actual
