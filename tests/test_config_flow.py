8ef5897ee064e7c0efa58328b9d4685662d2ab1f
Aaron Godfrey <me@aarongodfrey.dev>
2022-01-08 13:40:43 -0800

    Fixed tests

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -75,7 +75,7 @@ async def test_flow_user_step_steam_account_name_success(hass):
     }
     assert expected == result
     actual_config_entry: config_entries.ConfigEntry = result["result"]
-    assert "steam_wishlist" == actual_config_entry.unique_id
+    assert "steam_wishlist_1234567890" == actual_config_entry.unique_id
 
 
 async def test_flow_user_step_steam_profile_id_invalid(hass):
@@ -119,7 +119,7 @@ async def test_flow_user_step_steam_profile_id_success(hass):
     }
     assert expected == result
     actual_config_entry: config_entries.ConfigEntry = result["result"]
-    assert "steam_wishlist" == actual_config_entry.unique_id
+    assert "steam_wishlist_1234567890" == actual_config_entry.unique_id
 
 
 async def test_async_check_profile_id_valid_is_valid(mock_aioresponse):
