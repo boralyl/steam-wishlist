fd68ee7849fd4698cfc14095a118ff55c0a9543c
Aaron Godfrey <me@aarongodfrey.dev>
2024-08-14 20:07:30 -0700

    Fix deprecation warning.
    https://developers.home-assistant.io/blog/2022/07/08/config_entry_forwards/

--- custom_components/steam_wishlist/__init__.py
+++ custom_components/steam_wishlist/__init__.py
@@ -30,10 +30,7 @@ async def async_setup_entry(
             title=f"Steam Wishlist ({steam_id})",
         )
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.async_on_unload(entry.add_update_listener(update_listener))
 
