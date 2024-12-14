6b727ead2a4b83c7be514359fca7d95e5202740b
Aaron Godfrey <me@aarongodfrey.dev>
2024-12-14 10:57:15 -0800

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,6 @@ from aioresponses import aioresponses
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def auto_enable_custom_integrations(enable_custom_integrations):
-    """Enable custom integrations defined in the test dir."""
-    yield
-
-
 @pytest.fixture
 def mock_aioresponse():
     """Fixture to mock aiohttp calls."""
